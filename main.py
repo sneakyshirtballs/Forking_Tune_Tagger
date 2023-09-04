40bcf98528f7d22e7bb7ea5289f17eb01ab61f4f
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-05 02:35:28 +0600

    pass configs to music handler

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from utils.config_parser import parse_config_file
+from utils.custom_print import (p_status, p_terminate, p_warning, p_success)
+from modules.music import music_handler
 
 # Define the path to your configuration file
 file_path = "/home/hakuneko/Forking_Tune_Tagger_Test/media_config.txt"
 
 # Use the config_parser module to parse and validate the configuration file
-entries = parse_config_file(file_path)
+configs = parse_config_file(file_path)
 
-print(entries)
+media_type = configs["Media_Type"]
+p_success(f"Detected Media Type: {media_type}")
+
+# Call appropriate functions based on media_type
+match media_type:
+    case "Music":
+        music_handler.handle_music(configs)
+    case _:
+        p_terminate(f"{media_type} downloading is not available yet!")
