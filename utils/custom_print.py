74e4fe1446ecbad08632abcf2963ed26f3a22ae0
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-05 02:32:58 +0600

    add a status and success printing function

--- utils/custom_print.py
+++ utils/custom_print.py
@@ -1,11 +1,21 @@
 import sys
 
-def p_terminate(message):
-    print(message)
-    sys.exit(1)
+# ANSI escape codes for text colors
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RESET = '\033[0m'
+
 
 def p_status(message):
     print(f"[INFO] {message}")
 
+def p_terminate(message):
+    print(f"{RED}[ERROR] {message}{RESET}")
+    sys.exit(1)
+
+def p_success(message):
+    print(f"{GREEN}[SUCCESS] {message}{RESET}")
+
 def p_warning(message):
-    print(f"[WARNING] {message}")
+    print(f"{YELLOW}[WARNING] {message}{RESET}")
