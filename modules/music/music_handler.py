02d2397bde211c04ff09345dc2127e9c69582571
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-05 21:37:34 +0600

    update music handler with create_download_options() and add common utility functions

--- modules/music/music_handler.py
+++ modules/music/music_handler.py
@@ -1,4 +1,9 @@
 from utils.custom_print import (p_status, p_success, p_terminate, p_warning)
+from utils.common import (create_download_options)
 
 def handle_music(configs):
     media_type, download_format, queries = configs["Media_Type"], configs["Format"], configs["Queries"]
+    download_path = configs["Download_Path"]
+
+    p_status(f"Creating Download Options For {media_type}")
+    download_options = create_download_options(media_type, download_format, download_path)
---
+++ utils/common.py
@@ -0,0 +1,4 @@
+from utils.custom_print import (p_status, p_success, p_terminate, p_warning)
+
+def create_download_options(media_type, download_format, download_path):
+    p_success("Finished Downloading")
