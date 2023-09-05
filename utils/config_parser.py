2fa5f4b4eb753e1b83c31e80bd1e97b03cb71328
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-05 21:36:37 +0600

    update parser and validator for new option in media_config.txt named download_path

--- utils/config_parser.py
+++ utils/config_parser.py
@@ -1,27 +1,30 @@
 import re
 from utils.custom_print import (p_status, p_terminate, p_warning)
 from validators.media_config_validators import (
+    validate_download_path,
     validate_media_type,
     validate_resolution,
     validate_subtitles,
-    validate_format,
     validate_queries,
+    validate_format,
 )
 
 def parse_config_file(file_path):
-    pattern = r"Media_Type: (.+?)\nResolution: (.+?)\nSubtitles: (.+?)\nFormat: (.+?)\nQueries:\n([\s\S]+)"
+    pattern = r"(?:Download_Path: (.+?)\n)?Media_Type: (.+?)\nResolution: (.+?)\nSubtitles: (.+?)\nFormat: (.+?)\nQueries:\n([\s\S]+)"
+
     with open(file_path, 'r') as file:
         text = file.read()
     
     match = re.search(pattern, text)
     
     if match:
-        media_type, resolution, subtitles, download_format, queries = match.groups()
+        download_path, media_type, resolution, subtitles, download_format, queries = match.groups()
 
         # Split the queries string into a list by lines
         queries = queries.strip().split('\n')
         
         if all([
+            validate_download_path(download_path),
             validate_media_type(media_type),
             validate_resolution(resolution),
             validate_subtitles(subtitles),
@@ -29,6 +32,7 @@ def parse_config_file(file_path):
             validate_queries(queries)
         ]):
             return {
+                "Download_Path": download_path,
                 "Media_Type": media_type,
                 "Resolution": resolution,
                 "Subtitles": subtitles,
--- validators/media_config_validators.py
+++ validators/media_config_validators.py
@@ -1,4 +1,19 @@
-# validators/validators.py
+import os
+from utils.custom_print import p_terminate
+
+# Function to validate download path
+def validate_download_path(path):
+    # Check if the specified path exists
+    if not os.path.exists(path):
+        p_terminate(f"The specified path '{path}' does not exist.")
+    
+    # Check if 'media_config.txt' exists in the specified path
+    media_config_path = os.path.join(path, 'media_config.txt')
+    if not os.path.isfile(media_config_path):
+        p_terminate("The 'media_config.txt' file is not available in the specified path.")
+    
+    # If both checks pass, return True
+    return True
 
 # Function to validate media type
 def validate_media_type(value):
