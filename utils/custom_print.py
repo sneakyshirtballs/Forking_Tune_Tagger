60ace27eccb28e97433d4964e485648e071ddb8a
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-07 01:42:17 +0600

    update with delay parameter using time module

--- utils/custom_print.py
+++ utils/custom_print.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 
 # ANSI escape codes for text colors
 RED = '\033[91m'
@@ -7,15 +7,19 @@ YELLOW = '\033[93m'
 RESET = '\033[0m'
 
 
-def p_status(message):
+def p_status(message, delay=0):
     print(f"[INFO] {message}")
+    time.sleep(delay)
 
-def p_terminate(message):
+def p_terminate(message, delay=0):
     print(f"{RED}[ERROR] {message}{RESET}")
+    time.sleep(delay)
     sys.exit(1)
 
-def p_success(message):
+def p_success(message, delay=0):
     print(f"{GREEN}[SUCCESS] {message}{RESET}")
+    time.sleep(delay)
 
-def p_warning(message):
+def p_warning(message, delay=0):
     print(f"{YELLOW}[WARNING] {message}{RESET}")
+    time.sleep(delay)
