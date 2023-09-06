c665e543e3b6c62a06daed9bb566f59284f8004d
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-06 16:00:32 +0600

    add youtube download handler

---
+++ modules/common/yt_download_handler.py
@@ -0,0 +1,4 @@
+from utils.custom_print import (p_status, p_success, p_terminate, p_warning)
+
+def handle_yt_download(configs, create_download_options):
+    p_success("Imma handle youtube downloading for ya!")
