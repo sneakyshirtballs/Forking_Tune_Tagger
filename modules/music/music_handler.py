43a4173c01979081a51a6f18f9f80341520417cd
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-06 16:02:46 +0600

    pass configs & create_download_options to yt_download_handler

--- modules/music/music_handler.py
+++ modules/music/music_handler.py
@@ -1,9 +1,25 @@
 from utils.custom_print import (p_status, p_success, p_terminate, p_warning)
-from utils.common import (create_download_options)
+from utils.common import (write_file_outtmpl)
+from modules.common import yt_download_handler
 
 def handle_music(configs):
-    media_type, download_format, queries = configs["Media_Type"], configs["Format"], configs["Queries"]
-    download_path = configs["Download_Path"]
+    media_type, download_format, download_path = configs["Media_Type"], configs["Format"], configs["Download_Path"]
 
     p_status(f"Creating Download Options For {media_type}")
-    download_options = create_download_options(media_type, download_format, download_path)
+    
+    def create_download_options():
+        download_options = {
+        'format': 'bestaudio/best',
+        'outtmpl': write_file_outtmpl(download_path, download_format),
+        'download_archive': f"{download_path}/.download_history.txt",
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': download_format,
+        'preferredquality': '320',
+        }],
+        }
+        
+        return download_options
+
+    yt_download_handler.handle_yt_download(configs, create_download_options)
+
