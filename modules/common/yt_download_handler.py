12188627db81d3e653bc1928cb3882d578f28995
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-07 01:21:35 +0600

    add update_download_history function before starting to download queries

--- modules/common/yt_download_handler.py
+++ modules/common/yt_download_handler.py
@@ -1,4 +1,15 @@
 from utils.custom_print import (p_status, p_success, p_terminate, p_warning)
+from utils.common import update_download_history
 
 def handle_yt_download(configs, create_download_options):
-    p_success("Imma handle youtube downloading for ya!")
+    queries, media_type = configs["Queries"], configs["Media_Type"]
+
+    # Updates previous download history
+    download_path, download_format = configs["Download_Path"], configs["Format"]
+    update_download_history(download_path, download_format)
+    
+    p_status(f"Downloading {media_type} - Total {len(queries)} Queries")
+
+    p_status(queries)
+
+    p_status(f"Finished Downloading {media_type}")
