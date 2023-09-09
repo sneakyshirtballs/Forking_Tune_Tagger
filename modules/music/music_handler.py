440166762f3aeff962c2fc99488afe97a354e49e
SneakyShirtBalls <sneakyshirtballs@proton.me>
2023-09-09 06:08:25 +0600

    update music_handler with track detecting & updating its metadata capability

--- modules/music/music_handler.py
+++ modules/music/music_handler.py
@@ -1,9 +1,17 @@
-import os, requests, mutagen
+import os, spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
 from mutagen.mp4 import MP4, MP4Cover
 from shazamio import Shazam
-from utils.custom_print import (p_status, p_success, p_terminate, p_warning)
-from utils.common import (write_file_outtmpl)
+from utils.custom_print import ( p_success, p_warning)
+from utils.common import (write_file_outtmpl, download_thumbnail)
 from modules.common import yt_download_handler
+
+# Load environment variables from .env file
+load_dotenv()
+CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
 async def handle_music(configs):
     download_format, download_path = configs["Format"], configs["Download_Path"]
@@ -30,99 +38,82 @@ async def detect_music_and_embed_metadata(file_info):
 
     file_path, download_format = (file_info['file_path'], file_info['download_format'])    
     yt_title, yt_channel_name, yt_thumbnail = (file_info['title'], file_info['channel_name'], file_info['thumbnail'])
+    
+    yt_track_info = {
+                "title": yt_title,
+                "album": f"YouTube: {yt_channel_name}",
+                "artist": yt_channel_name,
+                "thumbnail": download_thumbnail(yt_thumbnail),
+            }
 
     try:
-        out = await shazam.recognize_song(file_path)
-        if "track" in out:
-            print(out)
-        else:
-            p_warning(f"Unable To Detect Song With Shazam: {file_path} {str(e)}")
-            p_warning(f"Embedding YouTube Metadata Instead!")
+        shazam_output = await shazam.recognize_song(file_path)
+        if "track" in shazam_output:
+            p_success(f"Successfully Shazamed {file_path}")
 
-            track_info = {
-                "title": yt_title,
-                "artist": yt_channel_name,
-                "thumbnail": yt_thumbnail,
-                "album": yt_title
+            shazamed_artist = shazam_output["track"]["subtitle"]
+            shazamed_track_title = shazam_output["track"]["title"]
+            shazam_track_info = {
+                "title": shazamed_track_title,
+                "album": f"Shazam: {shazamed_artist}",
+                "artist": shazamed_artist,
+                "thumbnail": download_thumbnail(shazam_output["track"]["images"]["coverart"])
             }
-            embed_with_mutagen(track_info)
+            
+            # Initialize Spotipy with the credentials
+            sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
+            try:
+                # Fetch track details
+                spotify_track_results = sp.search(q=f"track:{shazamed_track_title} artist:{shazamed_artist}",
+                                                  type='track', limit=1)
+
+                if spotify_track_results and 'tracks' in spotify_track_results and 'items' in spotify_track_results['tracks'] and len(spotify_track_results['tracks']['items']) > 0:
+                    spotify_track_info = spotify_track_results['tracks']['items'][0]
+                    #Extract desired information
+                    spotify_title = spotify_track_info['name']
+                    spotify_album = spotify_track_info['album']['name']
+                    spotify_artist_name = spotify_track_info['artists'][0]['name']
+                    spotify_cover_art = spotify_track_info['album']['images'][0]['url']
+
+                    spotify_track_info =  {
+                    'title': spotify_title,
+                    'album': spotify_album,
+                    'artist': spotify_artist_name,
+                    'thumbnail': download_thumbnail(spotify_cover_art),
+                    }
+                    p_success(f"Found Metadata From Spotify {file_path}")
+                    embed_with_mutagen(spotify_track_info, file_path)
+                
+                else:
+                    p_warning(f"Metadata Not Available On Spotify: {file_path}")
+                    p_warning(f"Embedding Shazamed Metadata Instead!")
+                    embed_with_mutagen(shazam_track_info, file_path)
+
+            except Exception as e:
+                p_warning(f"Metadata Not Found On Spotify: {file_path} {str(e)}")
+                p_warning(f"Embedding Shazamed Metadata Instead!")
+                embed_with_mutagen(shazam_track_info, file_path)
+
+        else:
+            p_warning(f"Unable To Detect Song With Shazam: {file_path}")
+            p_warning(f"Embedding YouTube Metadata Instead!")
+            embed_with_mutagen(yt_track_info, file_path)
 
     except Exception as e:
         p_warning(f"FAILED To Detect Song With Shazam: {file_path} {str(e)}")
         p_warning(f"Embedding YouTube Metadata Instead!")
+        embed_with_mutagen(yt_track_info, file_path)
 
-        track_info = {
-            "title": yt_title,
-            "artist": yt_channel_name,
-            "thumbnail": yt_thumbnail,
-            "album": yt_title
-        }
-        embed_with_mutagen(track_info)
+# Helper Function to Embed Metadata With Mutagen
+def embed_with_mutagen(track_info, file_path):
+    audio = MP4(file_path)
 
-# Helper Function
-def embed_with_mutagen(track_info):
-    m4a_file_path = "/home/hakuneko/Downloads/Detected Music/{48} {Alec Benjamin - Must Have Been The Wind (Lyrics)} {o9EBm4-9isI}.m4a"
+    audio['\xa9nam'] = track_info['title']
+    audio['\xa9ART'] = track_info['artist']    
+    audio['\xa9alb'] = track_info['album']
 
-    # Open the M4A file using mutagen
-    audio = mutagen.File(m4a_file_path)
-    if audio:
-        # Extract artist, title, and album
-        artist = audio.get('artist', [''])[0]
-        title = audio.get('title', [''])[0]
-        album = audio.get('album', [''])[0]
+    if track_info["thumbnail"]:
+        audio['covr'] = [MP4Cover(track_info["thumbnail"], MP4Cover.FORMAT_JPEG)]
 
-        # Extract other metadata if needed
-        duration = audio.info.length  # Duration in seconds
-        bitrate = audio.info.bitrate  # Bitrate in bits per second
-
-        # Print the extracted metadata
-        print(f"Artist: {artist}")
-        print(f"Title: {title}")
-        print(f"Album: {album}")
-        print(f"Duration: {duration} seconds")
-        print(f"Bitrate: {bitrate} bps")
-    else:
-        print("Invalid M4A file.")
-
-
-# Separate Function For Adding Metadata To Downloaded Music
-# async def recognize_and_update_metadata(directory_path, download_format):
-#     shazam = Shazam()
-    
-#     for filename in os.listdir(directory_path):
-#         if filename.endswith(download_format):
-#             file_path = os.path.join(directory_path, filename)
-#             try:
-#                 out = await shazam.recognize_song(file_path)
-#                 if 'track' in out:
-#                     track_info = out['track']
-#                     audio = MP4(file_path)
-#                     audio['title'] = track_info['title']
-#                     audio['artist'] = track_info['subtitle']
-                    
-#                     # Set the album name
-#                     audio['album'] = track_info['title']
-                    
-#                     # Set album art if available
-#                     cover_art_url = track_info['images']['coverart']
-
-#                     cover_art_data = download_cover_art(cover_art_url)
-#                     if cover_art_data:
-#                         audio['covr'] = [MP4Cover(cover_art_data, MP4Cover.FORMAT_JPEG)]
-
-#                     audio.save()
-#                     p_success(f"Updated metadata for {filename}")
-#                 else:
-#                     p_warning(f"Song recognition failed for {filename}")
-#             except Exception as e:
-#                 p_terminate(f"Error processing {filename}: {str(e)}")
-
-# def download_cover_art(cover_art_url):
-#     # Download cover art image using requests
-#     try:
-#         response = requests.get(cover_art_url)
-#         if response.status_code == 200:
-#             return response.content
-#     except Exception as e:
-#         p_terminate(f"Error downloading cover art: {str(e)}")
-#     return None
+    audio.save()
+    p_success(f"Embedded Metadata: {file_path}")
